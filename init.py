279e0d3002c1811dad2479064bb9d86069e55ba7
mritopep-logs <81800656+mritopep-logs@users.noreply.github.com>
2021-05-01 10:46:01 +0530

    Update init.py

--- init.py
+++ init.py
@@ -5,7 +5,13 @@ import pickle
 from general import make_dir, get_data, store_data, remove_dir, cred_store
 from paths import *
 
-if __name__ == "__main__":
+def init():
     make_dir(DATA_PATHS)
     make_dir(SCRIPT_PATHS)
     cred_store()
+    
+if __name__ == "__main__":
+    init()
+    
+    
+    
