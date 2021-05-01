521e6222da9bbb8ca7deda4fe5fd26b8facc4291
Antony J Malakkkaran <antonyjm462@gmail.com>
2021-05-01 09:53:11 +0530

    skull strip code update

--- shell_scripts/skull_strip.sh
+++ shell_scripts/skull_strip.sh
@@ -1,4 +1,4 @@
 source /work/miniconda/etc/profile.d/conda.sh &> ./logs/null.txt
 conda init bash &> ./logs/null.txt
 conda activate skull_strip &> ./logs/null.txt
-skull_strip.py -i $1 -o $2 &&  echo 'passed' > ./logs/$3 || echo 'failed' > ./logs/$3
+soft/skull_strip.py -i $1 -o $2 &&  echo 'passed' > ./logs/$3 || echo 'failed' > ./logs/$3
